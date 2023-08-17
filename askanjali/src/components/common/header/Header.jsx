3dd730019a797dcab80efd9282e22ef9e49edb8d
prashant kumar <prashant.k2901@gmail.com>
2023-08-17 10:42:44 +0530

    Notification system

--- askanjali/src/components/common/header/Header.jsx
+++ askanjali/src/components/common/header/Header.jsx
@@ -1,10 +1,41 @@
 import React, { useState } from "react"
 import { Link } from "react-router-dom"
+import { Switch, notification } from "antd"
 import Head from "./Head"
 import "./header.css"
 
 const Header = () => {
   const [click, setClick] = useState(false)
+  const [toggle,setoggle] = useState(false)
+
+  const toggler = ()=>{
+    toggle?setoggle(false):setoggle(true);
+
+    Notification.requestPermission().then(perm=>{
+      if(perm==="granted"){
+        if(!toggle){
+          const notification = new Notification("Welcome",{
+            body:"Nofitfications enabled",
+            data:"Hello There!",
+            // tag:"Welcome Message"
+          })
+          setInterval(()=>{
+            const interval = new Notification("Welcome",{
+              body:"Check out the Courses Available"
+            })
+          },3*60*60*1000)
+        }else{
+          const notification = new Notification("Disable",{
+            body:"Nofitfications Disable",
+            data:"Hello There!",
+            // tag:"Welcome Message"
+          })
+        }
+       
+        
+      }
+    })
+  }
 
   return (
     <>
@@ -24,8 +55,9 @@ const Header = () => {
             <li>
               <Link to='/pricing'>Pricing</Link>
             </li>
-            
+            <Switch onClick={toggler}/>
           </ul>
+
           <div className='start'>
             <div className='button'><Link to='/contact'>Profile</Link></div>
           </div>
--- askanjali/src/components/contact/Contact.jsx
+++ askanjali/src/components/contact/Contact.jsx
@@ -3,13 +3,19 @@ import Back from "../common/back/Back"
 import "./contact.css"
 import Header from "../common/header/Header"
 import "../../App.css"
+import { useNavigate } from "react-router-dom"
 
 const Contact = () => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    navigate("/")
+  }
 
   // const map = 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d904726.6131739549!2d85.24565535!3d27.65273865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1652535615693!5m2!1sen!2snp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" '
   return (
     <>
-      <Header/>
+      <Header />
       <Back title='Profile Detail' />
       <section className='contacts padding'>
         <div className='container shadow flexSB'>
@@ -19,7 +25,7 @@ const Contact = () => {
           <div className='right row'>
 
             <div className='items grid2'>
-              
+
               <h3>Personal Details</h3>
             </div>
 
@@ -40,13 +46,25 @@ const Contact = () => {
               <input type='text' placeholder='Description' />
               <input type='text' placeholder='Add link' />
               <button className='primary-btn'>Add project</button>
-              <br/>
+              <br />
               <h3>Experience</h3>
               <input type='text' placeholder='Position' />
               <input type='text' placeholder='Work Experience' />
               <button className='primary-btn'>Add work Experience</button>
-              <br/><br/>
-              <button className='primary-btn'>Update Details</button>
+              <br /><br />
+              <span>
+
+              </span>
+              <div className='items grid2'>
+
+                <div className='box'>
+                  <button className='primary-btn'>Update Details</button>
+                </div>
+                <div className='box'>
+                  <button className='primary-btn' onClick={logout}>Logout</button>
+                </div>
+
+              </div>
             </form>
 
           </div>
